6e4c4d0eb14412fd29351aed31d937a443478c7f
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-08-01 23:14:34 -0400

    added styling and a click listener to display character details

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,29 +1,24 @@
 // Write your Character component here
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import CharacterDetails from './CharacterDetails';
 
 const Character = props => {
     const { characters } = props;
 
+    const [info, setInfo] = useState(false);
+
     const StyledDiv = styled.div`
-        // background-color: white;
         border: 3px solid green;
         margin-bottom: 1rem;
         color: white;
     `;
 
-    const StyledList = styled.ul`
-        font-size: 1.2rem;
-        text-align: left;
-    `;
-
-
-
     return (
         <StyledDiv>
-            <h2>{characters.name}</h2>
-            <CharacterDetails characterDetails={characters} />
+            <h1>{characters.name}</h1>
+            {info === true ? <CharacterDetails characterDetails={characters} /> : false}
+            <button onClick={() => setInfo(!info)}>Info</button>
         </StyledDiv>
     );
 }
