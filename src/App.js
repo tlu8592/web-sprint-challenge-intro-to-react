05d7fd6cb78cac2c976d8d1711fe315bd96c147c
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-08-01 22:09:48 -0400

    fetched data with axios using effect hook, set data in state, rendered character component with data passed as props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characterData, setCharacterData] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        setCharacterData(res.data);
+      })
+      .catch(err => console.log(err))
+  }, []);
+
+  console.log(characterData);
+
+  const StyledHeader = styled.h1`
+    font-size: 2rem;
+  `;
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <StyledHeader className="Header">React Wars</StyledHeader>
+      {characterData.map(character => <Character key={character.name} characters={character} />)}
     </div>
   );
 }
